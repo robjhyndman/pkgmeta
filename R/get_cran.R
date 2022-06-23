8e2694d929d8fe840999ff3d8c27e5b3f61c7ae4
Rob J Hyndman <robjhyndman@gmail.com>
2022-06-23 13:12:08 +1000

    get_cran_packages returns description as well

--- R/get_cran.R
+++ R/get_cran.R
@@ -6,11 +6,9 @@
 # Examples
 # get_cran_packages("Hyndman")
 
-get_cran_packages <- function(
-    author,
-    include_downloads=FALSE,
-    start="2000-01-01"
-  ) {
+get_cran_packages <- function(author,
+                              include_downloads = FALSE,
+                              start = "2000-01-01") {
   packages <- pkgsearch::ps(author, size = 100) |>
     dplyr::filter(purrr::map_lgl(
       package_data, ~ grepl(author, .x$Author, fixed = TRUE)
@@ -35,54 +33,59 @@ get_cran_packages <- function(
 #' @export
 
 # Get meta data for vector of packages on CRAN
-get_meta_cran <- function(packages, include_downloads = FALSE, start = "2000-01-01") {
-  title <- version <- date <- authors <- github_url <- rep(NA_character_, length(packages))
-  url <- cran_url <- paste0("https://CRAN.R-project.org/package=",packages)
+get_meta_cran <- function(packages,
+                          include_downloads = FALSE,
+                          start = "2000-01-01") {
+  title <- version <- date <- desc <- authors <- github_url <- rep(NA_character_, length(packages))
+  url <- cran_url <- paste0("https://CRAN.R-project.org/package=", packages)
   for (i in seq_along(packages)) {
-    meta <- try(pkgsearch::cran_package(packages[i]), silent=TRUE)
-    if("try-error" %in% class(meta)) {
-      warning(paste(packages[i],"not on CRAN"))
+    meta <- try(pkgsearch::cran_package(packages[i]), silent = TRUE)
+    if ("try-error" %in% class(meta)) {
+      warning(paste(packages[i], "not on CRAN"))
     } else {
       date[i] <- meta$date
       title[i] <- meta$Title
+      desc[i] <- meta$Description
       version[i] <- meta$Version
       # Replace new line unicodes with spaces
-      authors[i] <- gsub("<U\\+000a>", " ", meta$Author, perl=TRUE)
+      authors[i] <- gsub("<U\\+000a>", " ", meta$Author, perl = TRUE)
       # Trim final period
-      authors[i] <- gsub("\\.$","",authors[i])
+      authors[i] <- gsub("\\.$", "", authors[i])
       if (!is.null(meta$URL)) {
-        urls <- gsub("\\n","",meta$URL)
+        urls <- gsub("\\n", "", meta$URL)
         urls <- stringr::str_split(urls, ",") |> unlist()
         # Remove CRAN urls as we already have them
-        iscran <- grep("cran", urls, ignore.case=TRUE)
-        if(length(iscran) > 0) {
+        iscran <- grep("cran", urls, ignore.case = TRUE)
+        if (length(iscran) > 0) {
           urls <- urls[-iscran]
         }
         # Identify github urls
-        isgithub <- grep ("github.com", urls, ignore.case=TRUE)
-        if(length(isgithub) > 0) {
+        isgithub <- grep("github.com", urls, ignore.case = TRUE)
+        if (length(isgithub) > 0) {
           github_url[i] <- (urls[isgithub])[1]
           urls <- urls[-isgithub]
         }
         # Store url. Either a non-cran and non-github url if it exists
-        if(length(urls) > 0)
+        if (length(urls) > 0) {
           url[i] <- urls[1]
-        # otherwise a github url
-        else if(length(isgithub) > 0)
+        } # otherwise a github url
+        else if (length(isgithub) > 0) {
           url[i] <- github_url[i]
+        }
         # otherwise just keep the cran url
       }
     }
   }
-  meta <- tibble::tibble(package = packages, date = date, title = title,
-                 version = version, authors = authors,
-                 url = url, cran_url = cran_url,
-                 github_url = github_url
+  meta <- tibble::tibble(
+    package = packages, date = date, title = title,
+    description = desc, version = version, authors = authors,
+    url = url, cran_url = cran_url,
+    github_url = github_url
   )
   # Remove missing packages
-  meta <- meta[!is.na(title),]
+  meta <- meta[!is.na(title), ]
   # Compute monthly downloads
-  if(include_downloads) {
+  if (include_downloads) {
     downloads <- cranlogs::cran_downloads(packages, from = start) |>
       dplyr::group_by(package) |>
       dplyr::mutate(cumsum = cumsum(count)) |>
@@ -93,7 +96,7 @@ get_meta_cran <- function(packages, include_downloads = FALSE, start = "2000-01-
         .groups = "drop"
       )
     # combine information
-    meta <- dplyr::left_join(meta, downloads, by="package")
+    meta <- dplyr::left_join(meta, downloads, by = "package")
   }
 
   # Fix some variable types
