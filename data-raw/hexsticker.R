f221ca8bf853c38dadc7ed1d3129264fe4ca6d5f
Rob J Hyndman <robjhyndman@gmail.com>
2025-04-07 16:52:32 +1000

    Tweaks to hex sticker

--- data-raw/hexsticker.R
+++ data-raw/hexsticker.R
@@ -21,7 +21,7 @@ R_img_cropped <- hex_crop(
 github_img_cropped <- hex_crop(
   images = here::here("data-raw","github-mark.png"),
   bg_fill = "#FFF",
-  border_colour = "#000",
+  border_colour = "#FFF",
   border_size = 15
 )
 
@@ -37,15 +37,15 @@ img_cropped <- hex_crop(
 ggplot() +
   xlim(0, 1) +
   ylim(0, 1) +
-  geom_image(aes(x = 0.5, y = 0.5), image = img_cropped, by = "height", size=0.9) +
-  geom_image(aes(x = 0.637, y = .73, image = R_img_cropped), size = 0.26) +
-  geom_image(aes(x = 0.495, y = 0.48, image = R_img_cropped), size = 0.26) +
-  geom_image(aes(x = 0.78, y = 0.48, image = github_img_cropped), size = 0.26) +
+  geom_image(aes(x = 0.5, y = 0.5), image = img_cropped, by = "height", size=0.99) +
+  geom_image(aes(x = 0.648, y = .737, image = R_img_cropped), size = 0.26) +
+  geom_image(aes(x = 0.520, y = 0.512, image = R_img_cropped), size = 0.26) +
+  geom_image(aes(x = 0.780, y = 0.512, image = github_img_cropped), size = 0.26) +
   annotate("text",
-         x = 0.5, y = .23, label = "pkgmeta", family = "firasans", size = 16, colour = "white",
+         x = 0.5, y = .26, label = "pkgmeta", family = "firasans", size = 24, colour = "white",
          fontface = "bold"
   ) +
-  coord_fixed() +
+  coord_cartesian(xlim = c(0,1), ylim = c(0,1), expand = FALSE) +
   theme_void()
 
 ggsave("./man/figures/pkgmeta-hex.png", height = 2.5, width = 2.5)
