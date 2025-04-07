1c44548349620f4bd1d4798636b2fd3a67caf8c5
Rob J Hyndman <robjhyndman@gmail.com>
2025-04-08 08:53:50 +1000

    Cache results by package

--- R/get_cran.R
+++ R/get_cran.R
@@ -38,78 +38,95 @@ get_cran_packages <- function(
   }
 }
 
-# Return meta data on CRAN packages
-#
 # Find meta data for a vector of CRAN packages and return as a tibble.
-get_meta_cran <- function(packages,
-                          include_downloads = FALSE,
-                          start = "2000-01-01",
-                          end = Sys.Date()) {
-  title <- version <- date <- desc <- authors <- github_url <- rep(NA_character_, length(packages))
-  url <- cran_url <- paste0("https://CRAN.R-project.org/package=", packages)
+get_meta_cran <- function(
+  packages,
+  include_downloads = FALSE,
+  start = "2000-01-01",
+  end = Sys.Date()
+) {
+  meta <- list()
   for (i in seq_along(packages)) {
-    meta <- try(pkgsearch::cran_package(packages[i]), silent = TRUE)
-    if ("try-error" %in% class(meta)) {
-      warning(paste(packages[i], "not on CRAN"))
-    } else {
-      date[i] <- meta$date
-      title[i] <- meta$Title
-      desc[i] <- meta$Description
-      version[i] <- meta$Version
-      # Replace new line unicodes with spaces
-      authors[i] <- gsub("<U\\+000a>", " ", meta$Author, perl = TRUE)
-      # Trim final period
-      authors[i] <- gsub("\\.$", "", authors[i])
-      if (!is.null(meta$URL)) {
-        urls <- gsub("\\n", "", meta$URL)
-        urls <- stringr::str_split(urls, ",") |> unlist()
-        # Remove CRAN urls as we already have them
-        iscran <- grep("cran", urls, ignore.case = TRUE)
-        if (length(iscran) > 0) {
-          urls <- urls[-iscran]
-        }
-        # Identify github urls
-        isgithub <- grep("github.com", urls, ignore.case = TRUE)
-        if (length(isgithub) > 0) {
-          github_url[i] <- (urls[isgithub])[1]
-          urls <- urls[-isgithub]
-        }
-        # Store url. Either a non-cran and non-github url if it exists
-        if (length(urls) > 0) {
-          url[i] <- urls[1]
-        } # otherwise a github url
-        else if (length(isgithub) > 0) {
-          url[i] <- github_url[i]
-        }
-        # otherwise just keep the cran url
-      }
+    meta[[i]] <- get_meta_cran_package(
+      packages[i],
+      include_downloads = include_downloads,
+      start = start,
+      end = end
+    )
+  }
+  meta <- dplyr::bind_rows(meta)
+  dplyr::arrange(meta, tolower(package))
+}
+
+# Return meta data on one CRAN package
+get_meta_cran_package <- function(
+  package,
+  include_downloads = FALSE,
+  start = "2000-01-01",
+  end = Sys.Date()
+) {
+  dest_folder <- tempdir()
+  dest_file <- paste0(dest_folder, "/", package)
+  if (include_downloads) {
+    dest_file <- paste0(dest_file, "_", start, "_", end)
+  }
+  dest_file <- paste0(dest_file, ".rds")
+  if (file.exists(dest_file)) {
+    return(readRDS(dest_file))
+  }
+  meta <- try(pkgsearch::cran_package(package), silent = TRUE)
+  if ("try-error" %in% class(meta)) {
+    warning(paste(package, "not on CRAN"))
+    return(NULL)
+  }
+  # Authors
+  # Replace new line unicodes with spaces
+  authors <- gsub("<U\\+000a>", " ", meta$Author, perl = TRUE)
+  # Trim final period
+  authors <- gsub("\\.$", "", authors)
+
+  # URLs
+  url <- cran_url <- paste0("https://CRAN.R-project.org/package=", package)
+  if (!is.null(meta$URL)) {
+    urls <- gsub("\\n", "", meta$URL)
+    urls <- stringr::str_split(urls, ",") |> unlist()
+    # Remove CRAN urls as we already have them
+    iscran <- grep("cran", urls, ignore.case = TRUE)
+    if (length(iscran) > 0) {
+      urls <- urls[-iscran]
+    }
+    # Identify github urls
+    isgithub <- grep("github.com", urls, ignore.case = TRUE)
+    if (length(isgithub) > 0) {
+      github_url <- (urls[isgithub])[1]
+      urls <- urls[-isgithub]
+    }
+    # Store url. Either a non-cran and non-github url if it exists
+    if (length(urls) > 0) {
+      url <- urls[1]
+    } else if (length(isgithub) > 0) { # otherwise a github url
+      url <- github_url
     }
   }
-  meta <- tibble::tibble(
-    package = packages, date = date, title = title,
-    description = desc, version = version, authors = authors,
-    url = url, cran_url = cran_url,
+
+  output <- tibble::tibble(
+    package = package,
+    date = as.Date(meta$date),
+    title = meta$Title,
+    description = meta$Description,
+    version = meta$Version,
+    authors = authors,
+    url = url,
+    cran_url = cran_url,
     github_url = github_url
   )
-  # Remove missing packages
-  meta <- meta[!is.na(title), ]
   # Compute monthly downloads
   if (include_downloads) {
-    downloads <- cranlogs::cran_downloads(packages, from = start, to = end) |>
-      dplyr::group_by(package) |>
-      dplyr::mutate(cumsum = cumsum(count)) |>
-      dplyr::summarise(
-        first_download = min(date[cumsum > 0]),
-        downloads = max(cumsum),
-        .groups = "drop"
-      )
-    downloads$first_download[downloads$downloads == 0] <- NA
-    # combine information
-    meta <- dplyr::left_join(meta, downloads, by = "package")
+    downloads <- cranlogs::cran_downloads(package, from = start, to = end)
+    output$first_download = min(downloads$date[downloads$count > 0])
+    output$downloads = sum(downloads$count, na.rm = TRUE)
   }
-
-  # Fix some variable types
-  meta$date <- as.Date(meta$date)
-
-  dplyr::arrange(meta, tolower(package))
+  # Save to file
+  saveRDS(output, dest_file)
+  return(output)
 }
