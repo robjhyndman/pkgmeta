4ef2fdb98d9db3c05e93736c80767ea2c8db8c2b
Rob J Hyndman <robjhyndman@gmail.com>
2025-04-07 15:03:29 +1000

    Added cran_packages argument

--- R/get_meta.R
+++ R/get_meta.R
@@ -10,6 +10,7 @@
 #' @param include_downloads Should total CRAN downloads since \code{start} be added to the tibble?
 #' @param start Start date for download statistics. Ignored if \code{include_downloads} is \code{FALSE}.
 #' @param end End date for download statistics. Ignored if \code{include_downloads} is \code{FALSE}.
+#' @param cran_packages Character vector of CRAN packages. Ignored if \code{cran_author} is provided.
 #' @param github_repos Character vector of github repos
 #' @param prefer_cran When a package is both on CRAN and github, which information should be preferenced?
 #' @examples
@@ -22,14 +23,17 @@ get_meta <- function(cran_author = NULL,
                      include_downloads = FALSE,
                      start = "2000-01-01",
                      end = Sys.Date(),
+                     cran_packages = NULL,
                      github_repos = NULL,
                      prefer_cran = TRUE) {
-  if(is.null(cran_author) & is.null(github_repos)) {
-    stop("At least one of cran_author and github_repos must be specified.")
+  if(is.null(cran_author) & is.null(github_repos) & is.null(cran_packages)) {
+    stop("At least one of cran_author, cran_packages and github_repos must be specified.")
   } else if(!is.null(cran_author)) {
     # Get CRAN packages that match the author
     #cran_author <- stringr::str_replace_all(cran_author, " ", "%20")
     cran_packages <- get_cran_packages(cran_author, include_downloads = include_downloads, start = start, end = end)
+  } else if(!is.null(cran_packages)) {
+    cran_packages <- get_meta_cran(cran_packages, include_downloads = include_downloads, start = start, end = end)
   }
   # Get github packages listed in repos
   if (!is.null(github_repos)) {
@@ -37,7 +41,7 @@ get_meta <- function(cran_author = NULL,
   }
 
   # Return results if only one of CRAN or github available
-  if(is.null(cran_author)) {
+  if(is.null(cran_packages)) {
     return(github_packages)
   } else if(is.null(github_repos)) {
     return(cran_packages)
